1871c731d3b714b053dafc8464da6a1b240bce7e
Tal Moshel <talmoshel444@gmail.com>
2024-12-17 20:04:07 +0200

    Update Army.java

--- src/main/java/com/example/cv/model/Army.java
+++ src/main/java/com/example/cv/model/Army.java
@@ -4,31 +4,25 @@ public class Army {
     private String period;
     private String body;
 
-    // Default constructor
     public Army() {}
 
-    // Parameterized constructor
     public Army(String period, String body) {
         this.period = period;
         this.body = body;
     }
 
-    // Getter for period
     public String getPeriod() {
         return period;
     }
 
-    // Setter for period
     public void setPeriod(String period) {
         this.period = period;
     }
 
-    // Getter for body
     public String getBody() {
         return body;
     }
 
-    // Setter for body
     public void setBody(String body) {
         this.body = body;
     }
