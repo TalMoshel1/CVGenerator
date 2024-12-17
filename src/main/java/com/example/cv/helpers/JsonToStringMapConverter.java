b8fa5cc855c725e8ec3650a43f97059a2f202bf6
Tal Moshel <talmoshel444@gmail.com>
2024-12-17 19:54:52 +0200

    Update JsonToStringMapConverter.java

--- src/main/java/com/example/cv/helpers/JsonToStringMapConverter.java
+++ src/main/java/com/example/cv/helpers/JsonToStringMapConverter.java
@@ -15,10 +15,8 @@ public class JsonToStringMapConverter {
     public static Map<String, Object> convertJsonToMap(JsonObject jsonObject) {
         Gson gson = new Gson();
 
-        // Convert JsonObject to JSON string using toJson() method
         String jsonString = gson.toJson(jsonObject);
 
-        // Convert the JSON string to a Map<String, Object>
         Type type = new TypeToken<Map<String, Object>>() {}.getType();
         return gson.fromJson(jsonString, type);  // This will return the Map
     }
